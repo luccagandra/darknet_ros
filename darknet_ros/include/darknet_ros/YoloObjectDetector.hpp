b7ac7f591aded979bef38fb45eb5a81d27a565b7
luccagandra <luccagandra@poli.ufrj.br>
2024-11-10 12:45:54 -0300

    push

--- darknet_ros/include/darknet_ros/YoloObjectDetector.hpp
+++ darknet_ros/include/darknet_ros/YoloObjectDetector.hpp
@@ -54,8 +54,6 @@ extern "C" {
 #include "parser.h"
 #include "region_layer.h"
 #include "utils.h"
-#include "blas.h"
-#include "darknet.h"
 }
 
 // Image interface.
@@ -63,7 +61,7 @@ extern "C" {
 
 extern "C" cv::Mat image_to_mat(image im);
 extern "C" image mat_to_image(cv::Mat m);
-//extern "C" int show_image(image p, const char* name, int ms);
+extern "C" int show_image(image p, const char* name, int ms);
 
 namespace darknet_ros {
 
